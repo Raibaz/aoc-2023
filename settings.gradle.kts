00566d655475030bf4335e2f695aff596ff72f76
GitHub Action <action@github.com>
2023-12-03 13:48:01 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,4 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
-rootProject.name = "Advent of Code Kotlin Template"
+rootProject.name = "aoc-2023"
 
 dependencyResolutionManagement {
     repositories {
